09278884eb37fe8e34474eda553a9cc4773c8ae2
GregoryKucherkov <kucherkovpromotion@gmail.com>
2023-12-04 21:33:59 -0500

    latest update

--- code-HW-1/main.py
+++ code-HW-1/main.py
@@ -3,9 +3,11 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
+
+
 def add_contact(contacts, args):
     if len(args) < 2:
-        return False
+        return "Contact was not added. The contact must be: <name phone number>"
     name, phone = args
     contacts[name] = phone
     return "Contact added."
@@ -19,17 +21,18 @@ def verify_contact(contacts, args):
     return False
 
 def change_contact(contacts, args):
+    if len(args) < 2:
+        return f"To change the contact input existing name and new phone. "
     if not verify_contact(contacts, args):
-        return False
+        return f"There is no such contact! To add contact use command 'Add'."
     name, phone = args
     contacts[name] = phone 
-    return True
+    return f"The contact {args[0]} has been changed successfully"
        
 def show_phone(contacts, name):
     if name not in contacts:
-        return False
-    
-    return contacts[name]
+        return "There is no such contact"
+    return f"{name}: {contacts[name]}"
         
     
 def show_all(contacts):
@@ -40,9 +43,9 @@ def show_all(contacts):
             print(f"{name}: {phone}")
 
 def print_supported_commands():
-    print(f"To add a contact, use command 'Add'.\n"
+    print(f"To add a contact, use command 'Add name phone'.\n"
       f"To end assistant, use command 'Close'.\n"
-      f"To change contact, use command 'Change'.\n"
+      f"To change contact, use command 'Change name new_phone. '.\n"
       f"To see a phone and a name, use command 'phone name'.\n"
       f"To see all saved names and phones, use command 'All'.")
 
@@ -51,42 +54,30 @@ def main():
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
+
+        if not user_input.strip():
+            print("No command entered. Please provide a command.")
+            continue
         command, *args = parse_input(user_input)
 
         if command in ["close", "exit", "bye", "stop"]:
             print("Good bye!")
             break
-
         elif command in ["hello", "hi", "wazzup", "morning", "greetings" ]:
             print("How can I help you?")
         elif command in ["add", "new contact", "new name"]:
             if verify_contact(contacts, args):
                 print(f"The contact is already exists")
-
-            elif add_contact(contacts, args):
-                print(f"Contact was successfully added")
-            else:
-                print(f"Contact was not added")
-        elif command == "change":
-            if verify_contact(contacts, args):    
-                if change_contact(contacts, args):
-                    print(f"The contact {args[0]} has been changed successfully")
-                else:
-                    print(f"The contact {args[0]} hasn't been changed")
+            print(add_contact(contacts, args))    
+        elif command in ["change", "change contact"]:
+            print(change_contact(contacts, args))
         elif command == "phone" and args[0]:
-            if phone := show_phone(contacts, args[0]):
-                print(f"{args[0]}: {phone}")
-            else:
-                print(f"There is no such contact")
-
+            print(show_phone(contacts, args[0]))
         elif command in ["all", "everybody"]:
             show_all(contacts)   
-
-
         elif command in ['?', 'help', 'how']:
             print_supported_commands()
         else:
-
             print("Invalid command.")
             print_supported_commands()
 
